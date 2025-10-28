51f9d6735e20c0d61933cd6e2d83c677d8bd8a1d
slkdfjlksajd <cesmith@usc.edu>
2025-10-27 23:16:56 -0700

    Update background.js

--- project-2/background.js
+++ project-2/background.js
@@ -1,17 +1,17 @@
-// function preload() {
-//   pondBG = loadImage('pond.jpg');
-//   pondSketch = loadImage('pondSketch.jpg');
-// }
+function preload() {
+  pondBG = loadImage('pond.jpg');
+  pondSketch = loadImage('pondSketch.jpg');
+}
 
-// function setup() {
-//   createCanvas(windowWidth, windowHeight);
-// }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
 
-// function draw() {
-//   background(pondBG);
-//   drawingContext.save();
-//   rect(0,0,mouseX, height);
-//   drawingContext.clip();
-//   image(pondSketch,0,0,width, height);
-//   drawingContext.restore();
-// }
+function draw() {
+  background(pondBG);
+  drawingContext.save();
+  rect(0,0,mouseX, height);
+  drawingContext.clip();
+  image(pondSketch,0,0,width, height);
+  drawingContext.restore();
+}
