bda164c86a2dc19ac7ed73a2abac459c347539b4
slkdfjlksajd <cesmith@usc.edu>
2025-10-28 10:31:35 -0700

    hi

---
+++ project-2/script.js
@@ -0,0 +1,7 @@
+<script>
+  const titleText = document.getElementById('h1');
+  const myAudio = document.getElementById('ambience');
+  titleText.addEventListener('click', function() {
+    ambience.play();
+  });
+</script>
