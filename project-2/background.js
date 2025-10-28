16d6f21d9117df2cf295282b609e2471fa4c8fbb
slkdfjlksajd <cesmith@usc.edu>
2025-10-27 23:02:15 -0700

    Update background.js

--- project-2/background.js
+++ project-2/background.js
@@ -7,11 +7,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
-  background(pondBG);
-  drawingContext.save();
-  rect(0,0,mouseX, height);
-  drawingContext.clip();
-  image(pondSketch,0,0,width, height);
-  drawingContext.restore();
-}
+// function draw() {
+//   background(pondBG);
+//   drawingContext.save();
+//   rect(0,0,mouseX, height);
+//   drawingContext.clip();
+//   image(pondSketch,0,0,width, height);
+//   drawingContext.restore();
+// }
